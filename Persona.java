86d617f405cd27558872358ab6e44334b53db5eb
Esteban Perez <estebanperezvergara@gmail.com>
2025-07-26 21:59:36 -0500

    Agreaga Person.java

---
+++ Persona.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private Interger edad;
+}
+
+public Persona (String nombre, String apellido, int edad){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+}
