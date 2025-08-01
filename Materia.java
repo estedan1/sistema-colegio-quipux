0290fdba90bd7232b25c5f993ec99b646a633de1
estedan1 <estebanperezvergara@gmail.com>
2025-07-31 23:51:02 -0500

    Update Materia.java

--- Materia.java
+++ Materia.java
@@ -1,13 +1,41 @@
 public class Materia {
-    private String nombre;
+    private String nombreDeMateria;
     private String codigo;
     private String profesorEncargado;
-    private float notas;
+    private double[] notas;
 
-    public Materia(String nombre, String codigo, String profesorEncargado, float notas) {
-        this.nombre = nombre;
+
+    public Materia(String nombreDeMateria, String codigo, String profesorEncargado, double [] notas) {
+        System.out.println("Se creo una nueva materia" + " " + nombreDeMateria + " " + codigo + " " + profesorEncargado);
+        this.nombreDeMateria = nombreDeMateria;
         this.codigo = codigo;
         this.profesorEncargado = profesorEncargado;
         this.notas = notas;
     }
+    public Materia() {
+        this("Sin nombre","a", "No definido",new double[]{0.1} );
+        this.nombreDeMateria = nombreDeMateria;
+        this.codigo = codigo;
+        this.profesorEncargado = profesorEncargado;
+    }
+
+    public Materia(String nombreDeMateria) {
+        this(nombreDeMateria,"a", "Juan",new double[]{0.1} );
+    }
+
+    public Materia (String nombreDeMateria,String codigo) {
+        this(nombreDeMateria, codigo, "No definido", new double[]{0.1});
+    }
+
+    public Materia (String nombreDeMateria,String codigo, String profesorEncargado) {
+        this(nombreDeMateria, codigo, profesorEncargado, new double[]{0.1});
+    }
+
+    public double[] getNotas(){
+    return this.notas;
 }
+    public double[] setNota(double[] notas) {
+    return notas;
+    }
+}
+
