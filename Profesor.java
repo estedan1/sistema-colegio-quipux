ff31d5ee8d799dc0b0e579f96482d1f14e2acb18
estedan1 <estebanperezvergara@gmail.com>
2025-07-31 23:50:25 -0500

    Update Profesor.java

--- Profesor.java
+++ Profesor.java
@@ -1,35 +1,49 @@
-public class Profesor extends Persona {
+public class Profesor extends Persona{
+    public String nombre;
+    public String apellido;
+    public int edad;
     public String materia;
-    public float jornada;
+    public Float jornada;
     public String estudianteEncargado;
 
-    public Profesor(String materia, float jornada, String estudianteEncargado) {
+
+    public Profesor(String nombre, String apellido, int edad, String materia, float jornada, String estudianteEncargado) {
+        System.out.println("Se creo un nuevo profesor " + nombre + " " + apellido + ' ' + "edad" + ' ' + edad + " " + materia + " " + ' ' + "jornada" + ' ' + jornada + " " + "Estudiante encargado" + " " + estudianteEncargado);
+        super(nombre,apellido,edad);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
         this.materia = materia;
         this.jornada = jornada;
         this.estudianteEncargado = estudianteEncargado;
-    }
-}
-public void enseñar(){
-    if enseñar() != true
-            System.out.printLn("No esta en clase enseñando");
-    else
-        System.out.printLn("Si esta en clase enseñando");
-}
 
-public void evaluar();{
-    switch (nota);
-    case value1
-    if nota < 1
-        System.out.printLn("perdio el año, es mas va perdiendo hasta el alma")
-    case value2;
-    if nota < 3
-        System.out.printLn("esta perdiendo, le toca recuperear")
-    case value3;
-    if nota < 4
-        System.out.printLn("estas bien pero no lo suficiente")
-    case value4;
-    if nota == 5
-        System.out.printLn("Exelente")
-    default;
-            System.out.printLn("todavia no tiene la nota")
-}
+    }
+
+    public void enseñar(boolean enseñar) {
+        if (enseñar == false) {
+            System.out.println("No esta en clase enseñando");
+        } else {
+            System.out.println("Si esta en clase enseñando");
+        }
+    }
+
+    public double[] getnota(){
+        Materia materia = new Materia("nombrnombreDeMateria", "codigo", "profesorEncargado", new double[]{0.0});
+        double[] nota = materia.getNotas();
+        return nota;
+    }
+    public void evaluar(float nota) {
+        if (nota <= 1) {
+            System.out.println("perdio el año, es mas va perdiendo hasta el alma");
+        } else if (nota > 1 && nota <= 2) {
+            System.out.println("esta perdiendo, le toca recuperar");
+        } else if (nota > 2 && nota <= 3) {
+            System.out.println("estas bien pero no lo suficiente");
+        } else if (nota == 5) {
+            System.out.println("Excelente");
+        } else {
+            System.out.println("todavia no tiene la nota");
+        }
+        }
+
+        }
