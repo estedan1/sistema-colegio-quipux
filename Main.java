ea846d5d3bc829d8991e6f2d4428ebff05daaa7a
estedan1 <estebanperezvergara@gmail.com>
2025-07-31 23:49:20 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 
 public class Main {
-    Alumno Juan = new Alumno();
-    Profesor Nelson = new Profesor();
-    Nelson.getNota();
-    Nelson.setNota();
-    Persona Cristian = new Persona();
+    public static void main(String[] args) {
 
+        Profesor creaProfesor = new Profesor("Nelson","Uribe",23,"Matematicas", 1.50F,"Esteban");
+        System.out.println("se creo el profesor");
 
+        Alumno creaAlumno = new Alumno("Esteban","Ceballos",16,"Emocional",180,"Nelson");
+        Materia creaMateria = new Materia("Español","q","Chucho", new double[]{4.1});
+    }
 
-    Materia Matematicas = new Materia();
 }
